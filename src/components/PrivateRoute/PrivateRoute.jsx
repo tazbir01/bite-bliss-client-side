8f663186617ba7c8c065b3eb9130151c9ebd34e0
tazbir01 <tajberahamad@gmail.com>
2023-10-18 21:29:06 +0600

    added private route

---
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { authContext } from "../Provider/AuthProvider";
+import {Navigate, useLocation} from "react-router-dom"
+const PrivateRoute = ({children}) => {
+    const {user, loader} = useContext(authContext)
+    const location = useLocation()
+
+    if(loader){
+        return <>
+            <div className="flex justify-center mt-20">
+            <span className="loading loading-spinner loading-lg"></span>
+            </div>
+        </>
+    }
+
+    if(user){
+        return children;
+    }
+    return <Navigate state={location.pathname} to="/login"></Navigate>
+};
+
+export default PrivateRoute;

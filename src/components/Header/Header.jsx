27b4162e96b651a85822e159433bf198ef961a15
tazbir01 <tajberahamad@gmail.com>
2023-10-19 16:18:13 +0600

    added six brand card

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,20 @@
 
 const Header = () => {
     return (
-        <div className="">
-            <h2 className="mt-20">this is header</h2>
+        <div className="mt-20 max-w-6xl mx-auto grid grid-cols-2 gap-5">
+            <div className="flex items-center">
+                <div className="text-right">
+                    <div className="flex justify-center">
+                    <img className="w-72" src="https://i.ibb.co/rQKZp5m/mockup-graphics-n-Xjv-Ul-E8418-unsplash-removebg-preview.png" alt="" />
+                    </div>
+                    <h1 className="text-6xl font-bold mb-5">Hi, from BiteBliss</h1>
+                    <p>BiteBliss: Your Ultimate Food and Beverage Paradise. Discover, savor, and indulge in a world of culinary delights. From savory dishes to exquisite beverages, we're your go-to source for all things food and drink. Join us on a delectable journey through the flavors of the world.</p>
+                    <button className="btn btn-info">Learn more</button>
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <img className="w-72 " src="https://i.ibb.co/HtxS7bd/mirko-fabian-y-S7-AJp5l-YWk-unsplash.jpg" alt="" />
+            </div>
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,11 @@
+import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
 const Home = () => {
     return (
-        <div className="h-[200vh] text-center">
+        <div className="">
             <Header></Header>
-            <h2 className="text-center ">this is home</h2>
+            <Footer></Footer>
         </div>
     );
 };
